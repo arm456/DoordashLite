1eda9bf40c5ffa88005e126d78ff9a21b190795d
arm456 <rashmitha.mary.a@gmail.com>
2021-04-07 22:23:09 -0700

    Fix issue with posting LiveData value

--- app/src/main/java/com/example/doordashlite/viewmodel/StoresFeedViewModel.kt
+++ app/src/main/java/com/example/doordashlite/viewmodel/StoresFeedViewModel.kt
@@ -1,25 +1,48 @@
 package com.example.doordashlite.viewmodel
 
 import android.util.Log
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
+import com.example.doordashlite.network.domain.Store
+import com.example.doordashlite.network.domain.StoreFeedResponseResult
 import com.example.doordashlite.repository.DoorDashStoreRepository
 import kotlinx.coroutines.launch
 
 class StoresFeedViewModel(private val repository: DoorDashStoreRepository) : ViewModel() {
 
-    fun getStoreFeedResponse() {
+    private val _storesLiveData = MutableLiveData<MutableList<Store>>()
+    val storesLiveData: LiveData<MutableList<Store>> = _storesLiveData
+
+    fun getStoreFeedResponse(offset: Int, limit: Int) {
         viewModelScope.launch {
-            repository.getDoorDashStoreFeed(37.422740f, -122.139956f, 10, 10)
+            repository.getDoorDashStoreFeed(37.422740f, -122.139956f, offset, limit)
+                .let { responseResult ->
+                    when (responseResult) {
+                        is StoreFeedResponseResult.Success -> {
+                            Log.d(StoresFeedViewModel::class.java.canonicalName, "Data fetched from backend")
+                            _storesLiveData.postValue(
+                                _storesLiveData.value.apply {
+                                    responseResult.storeFeed?.stores?.let {
+                                        this?.addAll(
+                                            it
+                                        )
+                                    }
+                                } ?: run { responseResult.storeFeed?.stores?.toMutableList() }
+                            )
+                        }
+                        else -> {
+                            Log.d(StoresFeedViewModel::class.java.canonicalName, "Data could not be fetched from backend")
+                        }
+                    }
+
+                }
         }
-        Log.d("DATA ","fetched")
+
     }
 
     class Factory(private val repository: DoorDashStoreRepository) : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-           return StoresFeedViewModel(repository) as T
+            return StoresFeedViewModel(repository) as T
         }
     }
 }
